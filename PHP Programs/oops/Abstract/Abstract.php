3688304e1d9ccba93837a7293c7822b9756ce533
Hemanth Kumar <90673550+Hemanthoo7@users.noreply.github.com>
2022-07-21 19:00:07 +0530

    Add files via upload

---
+++ PHP Programs/oops/Abstract/Abstract.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract function bank{
+  abstract function id_prof();
+}
+
+class sbi extends bank{
+  function test()
+  {
+    echo "Test Function";
+  }
+}
+
+?>
---
+++ PHP Programs/oops/Constructor/Student.php
@@ -0,0 +1,20 @@
+<?php
+class Student{
+    public $name;
+    public $class;
+    function __construct($name, $class){
+        $this->name = $name;
+        $this->class = $class;
+    }
+    function properties(){
+        echo "$this->name is studying at $this->class grade.".PHP_EOL;
+    }
+}
+$student =new Student();
+$student->name= "Inzi,Turzo";
+$student->class=12;
+echo '<br>';
+$student->properties();
+$student = new Student("Inzi,Turzo","12");
+$student->properties();
+?>
